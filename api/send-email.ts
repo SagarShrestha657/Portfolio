83b0a5ef55baa4594827f51add2f2010192852ec
SagarShrestha657 <shresthasagar657@gmail.com>
2025-07-31 12:42:24 +0530

    fixed email error

--- api/send-email.ts
+++ api/send-email.ts
@@ -1,14 +1,17 @@
 import nodemailer from 'nodemailer';
 
-export async function handler(req: any, res: any) {
+export default async function handler(req: any, res: any) {
+  console.log("1")
   if (req.method !== 'POST') {
     res.status(405).json({ error: 'Method not allowed' });
     return;
   }
+  console.log("2")
   const { name, email, message } = req.body;
   if (!name || !email || !message) {
     return res.status(400).json({ error: 'All fields are required.' });
   }
+  console.log("3")
   try {
     const transporter = nodemailer.createTransport({
       service: 'gmail',
@@ -17,6 +20,7 @@ export async function handler(req: any, res: any) {
         pass: process.env.EMAIL_PASS,
       },
     });
+    console.log("4")
     await transporter.sendMail({
       from: process.env.EMAIL_USER,
       to: process.env.EMAIL_USER,
@@ -24,6 +28,7 @@ export async function handler(req: any, res: any) {
       text: message,
       replyTo: email,
     });
+    console.log("5")
     res.status(200).json({ success: true, message: 'Email sent successfully!' });
   } catch (error) {
     res.status(500).json({ error: 'Failed to send email.' });
